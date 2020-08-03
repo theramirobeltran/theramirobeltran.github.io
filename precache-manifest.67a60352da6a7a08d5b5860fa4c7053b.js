d1b736822e146da00be50b958a54f85d37cbefd4
Ramiro Beltran <beltran.ramiro@hotmail.com>
2020-08-03 17:40:00 -0500

    Updates

---
+++ precache-manifest.67a60352da6a7a08d5b5860fa4c7053b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86598bc1732a463e2fa50bf8a4b23bdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e993a2ac9a3872b1e947",
+    "url": "/static/css/main.090cbb5c.chunk.css"
+  },
+  {
+    "revision": "b747627c37825efb649d",
+    "url": "/static/js/2.cd5f9d9d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cd5f9d9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e993a2ac9a3872b1e947",
+    "url": "/static/js/main.28a80a1e.chunk.js"
+  },
+  {
+    "revision": "16d50208a04408d1bf24",
+    "url": "/static/js/runtime-main.23041e79.js"
+  },
+  {
+    "revision": "7789b5d62e8af5a9f9c0d87144c2c19e",
+    "url": "/static/media/profile_pic.7789b5d6.jpg"
+  }
+]);
