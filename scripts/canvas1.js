f79fa143b5fa9d8204918025f41dd451d1500010
Ramiro Beltran <beltran.ramiro@hotmail.com>
2020-08-03 12:32:07 -0500

    setup: multipage

---
+++ scripts/canvas1.js
@@ -0,0 +1,10 @@
+let ctx;
+
+!function init() {
+    const canvas = document.getElementById('canvas1');
+    // canvas.width = window.innerWidth;
+    // canvas.height = '500';
+
+    ctx = canvas.getContext('2d');
+    // ctx.fillRect(0,0, canvas.width, canvas.height);
+}()
